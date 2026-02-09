f39b716a743b7d690021053364b33eee830a919c
NSSKM <kirubasumathi45@gmail.com>
2026-02-09 14:46:44 +0530

    update-new

--- app/acceptable-use/page.tsx
+++ app/acceptable-use/page.tsx
@@ -19,31 +19,31 @@ export default function AcceptableUsePolicy() {
         IMPORTANT:
       </p>
 
-      <p className="mb-4">
+      <p className="mb-12">
        Please read this Terms of Service Agreement  before clicking the “accept” button, and/or using the Foo Monk, LLC (“Findlead”) software-as-a-service product, application programming interface, or other services that accompany or are provided in connection with this Agreement. Subscriber should review Findlead’s <Link href="https://instantly.ai/technical-parameters-addendum">Technical Parameters Addendum</Link>, which is hereby incorporated by reference and may be updated from time-to-time in Findlead’s sole discretion (together with the Terms of Service Agreement, the “Agreement”), in detail to understand the specific ways in which the Findlead Service reports certain data back to Subscriber, as well as certain dependencies on the functionality of the Findlead Service. By clicking the “Agree” button, and/or using the Findlead Service (as defined below) in any way, you and the entity that you represent (“Subscriber”) is unconditionally consenting to be bound by and is becoming a party to this Agreement with Findlead and Subscriber represent and warrant that Subscriber has the authority to bind such entity to these terms. If Subscriber does not unconditionally agree to all of the terms of this Agreement, use of the Findlead Service is strictly prohibited. 
 
       </p>
 
      
 
-      <p className="mb-4">
+      <p className="mb-12">
         Please note that Findlead may modify or amend this Agreement at any
         time. When Findlead modifies or amends this Agreement, Findlead will
         update the last updated date above, and Findlead may send an email
         to the last email address provided by Subscriber to Findlead.
       </p>
 
-      <p className="mb-4">
+      <p className="mb-12">
         Findlead may require Subscriber to provide consent to the updated Agreement in a specified manner before any further use of the Findlead Service is permitted. If Subscriber does not agree to any change(s) after receiving a notice of such change(s), Subscriber must stop using the Findlead Service. Otherwise, Subscriber’s continued use of the Findlead Service constitutes its acceptance of such change(s).  Please regularly check this Agreement.
       </p>
 
 
-      <p className="mb-4">
+      <p className="mb-12">
         <strong>‍Section 12 (Arbitration Agreement) contains provisions that govern how to resolve disputes between Subscriber and Findlead. Among other things, section 12 (Arbitration Agreement) includes an agreement to arbitrate which requires, with limited exceptions, that all disputes between Subscriber and Findlead shall be resolved by binding and final arbitration. Section 12 also contains a class action and jury trial waiver. Please read Section 12 (Arbitration Agreement) carefully.</strong>
       </p>
 
 
-<p className="mb-4">‍If Subscriber purchases any feature or functionality of the Findlead Service for a term (the <strong>“Initial Order Term”</strong>), then the subscription will be automatically renewed for additional periods of the same duration as the Initial Order Term at Findlead’s then current fee for such features and functionality unless Subscriber opts out of the automatic renewal in accordance with Section 5.1(c) below.
+<p className="mb-12">‍If Subscriber purchases any feature or functionality of the Findlead Service for a term (the <strong>“Initial Order Term”</strong>), then the subscription will be automatically renewed for additional periods of the same duration as the Initial Order Term at Findlead’s then current fee for such features and functionality unless Subscriber opts out of the automatic renewal in accordance with Section 5.1(c) below.
 </p>
 
 
